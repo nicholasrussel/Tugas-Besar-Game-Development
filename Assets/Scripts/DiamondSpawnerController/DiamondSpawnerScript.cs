3c7af60dc6f85d5867749cd19a79b8eb1097eb9f
edwanlimanjaya <wanlim0810@gmail.com>
2022-11-06 16:07:37 +0700

    fourth_commit

--- Assets/Scripts/DiamondSpawnerController/DiamondSpawnerScript.cs
+++ Assets/Scripts/DiamondSpawnerController/DiamondSpawnerScript.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class DiamondSpawnerScript : MonoBehaviour
 {
     public GameObject[] diamondPrefabs;
-    private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    public int time = 600;
+    // private float startDelay = 3;
+    // private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomDiamond", startDelay, spawnInterval);
+        // InvokeRepeating("SpawnRandomDiamond", startDelay, spawnInterval);
+        SpawnRandomDiamond();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void SpawnRandomDiamond()
     {
-        int diamondIndex = Random.Range(0, diamondPrefabs.Length);
-        Instantiate(diamondPrefabs[diamondIndex], diamondPrefabs[diamondIndex].transform.position, Quaternion.identity);
+        // int diamondIndex = Random.Range(0, diamondPrefabs.Length);
+        // Instantiate(diamondPrefabs[diamondIndex], diamondPrefabs[diamondIndex].transform.position, Quaternion.identity);
+        
+        while (time!=0)
+        {
+            if (time == 360)
+            {
+                Instantiate(diamondPrefabs[2], diamondPrefabs[2].transform.position, Quaternion.identity);
+        
+            }else if (time == 230)
+            {
+                Instantiate(diamondPrefabs[0], diamondPrefabs[0].transform.position, Quaternion.identity);
+        
+            }else if (time == 100)
+            {
+                Instantiate(diamondPrefabs[1], diamondPrefabs[1].transform.position, Quaternion.identity);
+            }
+            time--;
+        }
+        
     }
 }
