bee96c3fee2ea934ab1baa65e184ec24d8975b40
edwanlimanjaya <wanlim0810@gmail.com>
2022-12-12 11:08:13 +0700

    commit_terakhir_dua_dalamNamaTuhanYesus

--- Assets/Scripts/SpawnPesanan.cs
+++ Assets/Scripts/SpawnPesanan.cs
@@ -6,7 +6,7 @@ public class SpawnPesanan : MonoBehaviour
 {
     public GameObject[] pesananPrefabs;
     private float startDelay = 2;
-    private float spawnInterval = 2f;
+    private float spawnInterval = 5f;
     public float numberOfClone = 0;
 
     public static SpawnPesanan instance;
